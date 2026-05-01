a0cb77d0743dab22fe35bbd504ecebf1a9c18cd7
sourav-gls <souravghosh.gls@gmail.com>
2026-05-01 09:34:32 +0600

    work on img error

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images:{
-    domains:["pixabay.com"] ,
-  } ,
+ images: {
+  domains: ["images.unsplash.com"],
+} ,
 };
 
 export default nextConfig;
