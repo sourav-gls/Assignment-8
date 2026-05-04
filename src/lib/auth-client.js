a51913c7f15886b5d57a4663acc5b569110b5e35
sourav-gls <souravghosh.gls@gmail.com>
2026-05-04 23:31:01 +0600

    final check

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     
-    baseURL: "http://localhost:3000"
+    baseURL: "https://assignment-8-omega-nine.vercel.app"
 })
 
