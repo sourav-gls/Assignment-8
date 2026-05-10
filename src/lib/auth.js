ee7cfa482814b352208a6aa4ebdfe0ec31c79a96
sourav-gls <souravghosh.gls@gmail.com>
2026-05-10 09:01:05 +0600

    work on deploy error

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,24 +2,31 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-
 const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db("sourav");
 
-
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
-    
     client
   }),
+
+  
+  baseURL: process.env.BETTER_AUTH_URL,
+  secret: process.env.BETTER_AUTH_SECRET,
+
+  trustedOrigins: [
+    process.env.BETTER_AUTH_URL,
+    "http://localhost:3000",
+  ],
+
   emailAndPassword: {
-    enabled:true
+    enabled: true
   },
-    socialProviders: {
+
+  socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
-      
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }
   }
 });
